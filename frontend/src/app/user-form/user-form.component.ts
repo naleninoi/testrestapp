024228f4d41ca712cc3cafd2e35f4bbde7ceef12
Viktor K.-A <61115892+naleninoi@users.noreply.github.com>
2021-02-19 11:41:58 +0300

    Add files via upload

---
+++ frontend/src/app/user-form/user-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../user-service';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+
+  user: User;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService) {
+    this.user = new User();
+  }
+
+  onSubmit(): void {
+    this.userService.save(this.user)
+      .subscribe(result => this.gotoUserList());
+  }
+
+  gotoUserList(): void {
+    this.router.navigate(['/users']);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
